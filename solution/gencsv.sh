34a8fd76cf0250fdbcb49cd6484ab274ad049e81
Abhinav Kumar <abhinav2506@gmail.com>
2021-06-06 14:06:12 +0000

    part 1 solution

---
+++ solution/gencsv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]
+then
+    totaliter=10
+elif [[ $# -eq 1 ]]
+then
+    totaliter=$1
+else
+    totaliter=0
+fi
+
+currentDir=`dirname $0`
+outputFile="${currentDir}/inputFile"
+
+if [[ -f ${outputFile} ]]
+then
+    rm ${outputFile}
+fi
+
+for ((i = 1 ; i <= $totaliter; i++))
+do
+    echo "$i, $RANDOM" >> ${outputFile}
+done
